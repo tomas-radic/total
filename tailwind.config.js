88e423411ea084823dee36790ae6ef4fe5bba265
dino <tomas.radic@gmail.com>
2021-09-30 17:16:41 +0200

    Responsive homepage template

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Raleway', 'sans-serif']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
