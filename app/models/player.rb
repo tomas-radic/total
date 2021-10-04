97f924a81510583d5b6f06eee7bf6ac7cd802eba
dino <tomas.radic@gmail.com>
2021-10-04 11:41:33 +0200

    Enabled Devise trackable for Player model

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,7 +2,7 @@ class Player < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :trackable
 
 
   # Relations -----
--- db/schema.rb
+++ db/schema.rb
@@ -68,6 +68,11 @@ ActiveRecord::Schema.define(version: 2021_10_02_211741) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_players_on_email", unique: true
