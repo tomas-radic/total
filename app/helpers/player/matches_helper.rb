5a859d0376104eedf3f91a48ae6552173532c4ef
dino <tomas.radic@gmail.com>
2021-10-23 11:27:25 +0200

    Removed unused parameter from helper method.

--- app/helpers/player/matches_helper.rb
+++ app/helpers/player/matches_helper.rb
@@ -1,6 +1,6 @@
 module Player::MatchesHelper
 
-  def link_to_match(match, current_player, &block)
+  def link_to_match(match, &block)
     link_to match_path(match), class: "underline" do
       if block_given?
         yield
