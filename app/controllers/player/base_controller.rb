182ab3bbb1b8a49ff22c809a8f2168d5cc8a8841
dino <tomas.radic@gmail.com>
2021-12-12 11:17:35 +0100

    Sign out anonymized player everytime.

--- app/controllers/player/base_controller.rb
+++ app/controllers/player/base_controller.rb
@@ -1,5 +1,16 @@
 class Player::BaseController < ApplicationController
 
   before_action :authenticate_player!
+  before_action :sign_out_anonymized!
+
+
+  private
+
+  def sign_out_anonymized!
+    if current_player.anonymized_at.present?
+      sign_out current_player
+      redirect_to root_path
+    end
+  end
 
 end
