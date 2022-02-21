4238fcd94f2cfe71992a370bb015029bcd8602dc
dino <tomas.radic@gmail.com>
2022-02-21 23:02:32 +0100

    Added avg_matches_monthly method to Enrollment model.

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -11,4 +11,28 @@ class Enrollment < ApplicationRecord
 
   # Scopes -----
   scope :active, -> { where(canceled_at: nil) }
+
+
+  def avg_matches_monthly
+    return @avg_matches_monthly unless @avg_matches_monthly.nil?
+
+    month_beginning = created_at.to_date.beginning_of_day
+    month_end = month_beginning + 1.month
+    end_of_time = Time.now
+    end_of_time = season.ended_at if season.ended_at && season.ended_at < end_of_time
+
+    matches = player.matches.reviewed.where(competitable: season)
+    matches_counts = []
+
+    while month_end <= end_of_time
+      matches_to_count = matches.where("reviewed_at >= ? and reviewed_at < ?", month_beginning, month_end)
+      matches_counts << matches_to_count.count
+      month_beginning += 1.month
+      month_end += 1.month
+    end
+
+    if matches_counts.any?
+      @avg_matches_monthly = matches_counts.inject(0) { |sum, c| sum += c } / matches_counts.length
+    end
+  end
 end
--- spec/models/enrollment_spec.rb
+++ spec/models/enrollment_spec.rb
@@ -1,5 +1,92 @@
-# require 'rails_helper'
-#
-# RSpec.describe Enrollment, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+require 'rails_helper'
+
+RSpec.describe Enrollment, type: :model do
+
+  describe "Instance methods" do
+
+    describe "avg_matches_monthly" do
+      subject { enrollment.avg_matches_monthly }
+
+      let!(:enrollment) { create(:enrollment, created_at: Date.today - 2.months - 10.days) }
+
+      context "Without any matches" do
+        it "Returns nil" do
+          expect(subject).to eq(0)
+        end
+      end
+
+      context "With 1 reviewed match within 1st month, 3 reviewed matches within second month and 1 reviewed match within uncompleted last month" do
+        let!(:another_player) { create(:player, seasons: [enrollment.season]) }
+
+        let!(:match1) { create(:match, :reviewed, competitable: enrollment.season,
+                              reviewed_at: enrollment.created_at + 5.days,
+                               assignments: [
+                                 build(:assignment, side: 1, player: enrollment.player),
+                                 build(:assignment, side: 2, player: another_player )
+                               ]) }
+
+        let!(:match_f1) { create(:match, :finished, competitable: enrollment.season,
+                                finished_at: enrollment.created_at + 5.days,
+                              assignments: [
+                                build(:assignment, side: 1, player: enrollment.player),
+                                build(:assignment, side: 2, player: another_player )
+                              ]) }
+
+        let!(:match_f2) { create(:match, :finished, competitable: enrollment.season,
+                                finished_at: enrollment.created_at + 5.days,
+                                assignments: [
+                                  build(:assignment, side: 1, player: enrollment.player),
+                                  build(:assignment, side: 2, player: another_player )
+                                ]) }
+
+        let!(:match2) { create(:match, :reviewed, competitable: enrollment.season,
+                              reviewed_at: enrollment.created_at + 35.days,
+                              assignments: [
+                                build(:assignment, side: 1, player: enrollment.player),
+                                build(:assignment, side: 2, player: another_player )
+                              ]) }
+
+        let!(:match3) { create(:match, :reviewed, competitable: enrollment.season,
+                              reviewed_at: enrollment.created_at + 35.days,
+                              assignments: [
+                                build(:assignment, side: 1, player: enrollment.player),
+                                build(:assignment, side: 2, player: another_player )
+                              ]) }
+
+        let!(:match4) { create(:match, :reviewed, competitable: enrollment.season,
+                              reviewed_at: enrollment.created_at + 35.days,
+                              assignments: [
+                                build(:assignment, side: 1, player: enrollment.player),
+                                build(:assignment, side: 2, player: another_player )
+                              ]) }
+
+        let!(:match5) { create(:match, :reviewed, competitable: enrollment.season,
+                              reviewed_at: enrollment.created_at + 65.days,
+                              assignments: [
+                                build(:assignment, side: 1, player: enrollment.player),
+                                build(:assignment, side: 2, player: another_player )
+                              ]) }
+
+        it "Returns average number of reviewed matches monthly: 2" do
+          expect(subject).to eq(2)
+        end
+
+        context "When 1st month has not yet been reached" do
+          before { allow(Time).to receive(:now).and_return(enrollment.created_at + 10.days) }
+
+          it "Return nil" do
+            expect(subject).to be_nil
+          end
+        end
+
+        context "When season ended after 1st month" do
+          before { enrollment.season.update_column(:ended_at, enrollment.created_at + 1.month) }
+
+          it "Returns average number of reviewed matches monthly: 2" do
+            expect(subject).to eq(1)
+          end
+        end
+      end
+    end
+  end
+end
