01e6048045abe937ecd836de5bc477e647cd40f2
dino <tomas.radic@gmail.com>
2022-04-27 11:11:52 +0200

    Changed order of players on manager page.

--- app/controllers/manager/pages_controller.rb
+++ app/controllers/manager/pages_controller.rb
@@ -1,7 +1,7 @@
 class Manager::PagesController < Manager::BaseController
 
   def dashboard
-    @players = Player.all.order(:name).includes(:enrollments)
+    @players = Player.all.order(created_at: :desc).includes(:enrollments)
   end
 
 end
