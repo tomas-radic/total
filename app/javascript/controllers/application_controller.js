672bea412ea590e06cdf181d7f890b42a8caa502
dino <tomas.radic@gmail.com>
2022-05-10 17:05:37 +0200

    Autoreload also 300ms after stopped scrolling page.

--- app/javascript/controllers/application_controller.js
+++ app/javascript/controllers/application_controller.js
@@ -9,6 +9,18 @@ export default class extends Controller {
     this.loadTimeTarget.textContent = Math.floor(Date.now() / 1000)
 
     window.addEventListener("focus", this.autoreload);
+
+
+    var timer = null;
+    const ar = this.autoreload;
+
+    window.addEventListener('scroll', function() {
+      if (timer !== null) {
+        clearTimeout(timer);
+      }
+
+      timer = setTimeout(() => ar(), 300);
+    }, false);
   }
 
 
